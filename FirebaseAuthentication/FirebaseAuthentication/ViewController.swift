339bf9386299488951c9a1a1802ad06e06c2f67f
Christopher Becom <cnbecom@gmail.com>
2018-06-27 23:02:30 -0400

    If applied, this commit adds the initial project file

---
+++ FirebaseAuthentication/FirebaseAuthentication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseAuthentication
+//
+//  Created by Christopher Becom on 6/27/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
